56b40773ea1b4769babe7b59e578aac1b8f90147
Giovanni86 <giovanni.borda@gmail.com>
2016-10-14 11:04:26 -0500

    abances generales en todo el sitio

---
+++ made htmls/madehtmls.php
@@ -0,0 +1,114 @@
+<!-- ¿Qué es Aliados 10? -->
+<h3>¿Qué es Aliados 10?</h3>
+<h4>¿De dónde surge el Proyecto Aliados 10?</h4>
+<p>El Proyecto surge del interés por generar un trabajo colaborativo y colectivo entre las instituciones educativas de la nación, iniciando con la creación de binas de trabajo que permitan compartir sus experiencias exitosas en educación que permitan el mejoramiento de aspectos directivos, académicos, pedagógicos y comunitarios.</p>
+<p>Se trata entonces de aunar esfuerzos pedagógicos, didácticos, y administrativos mediante la construcción de planes de acompañamiento que se desarrollen entre las parejas de instituciones educativas.</p>
+<p>Así mismo, se plantea recuperar el recorrido por algunas experiencias en la implementación de estrategias de acompañamiento previas, a fin de potenciar el trabajo que algunos colegios han reconocido como importante, esto mediante la potenciación de líderes académicos que apoyen el desarrollo entre pares.</p>
+<p>Entre estas posibilidades se resaltan el desarrollo de capacidades internas de las escuelas, la expansión y diversificación de las oportunidades de aprendizaje, el manejo eficiente de recursos y el fomento del <a href="http://cambridge.academia.edu/MarianoRosenzvaig.">liderazgo pedagógico.</a></p>
+<h4>¿Cuál es el propósito de Aliados 10?</h4>
+<p>Consolidar una red de trabajo entre las instituciones mentoras y acompañadas para la mejora de los procesos de formación en la educación básica y media, manifestado en la obtención de mejores resultados de las pruebas de Estado y en la cualificación de la comunidad educativa. Esto implica que directivos, docentes, administrativos, estudiantes y padres de familia puedan participar de Aliados 10, haciendo un especial énfasis en la función directiva y en la docencia.</p>
+<p>Este propósito cuenta con los siguientes objetivos:</p>
+<ul>
+	<li>Facilitar el intercambio de conocimientos, estrategias y buenas prácticas entre los establecimientos educativos que conformen parejas de acompañamiento.</li>
+	<li>Promover el acompañamiento pedagógico entre directivos docentes y docentes de colegios privados o públicos de alto desempeño e IE oficiales de desempeño medio.</li>
+	<li>Contribuir al logro de cambios en la cultura institucional de IE acompañadas, con base en la experiencia compartida por los colegios mentores.</li>
+	<li>Apoyar a las IE acompañadas para que mejoren el uso pedagógico de la información institucional.</li>
+	<li>Promover la implementación de estrategias didácticas para el desarrollo pedagógico y para la evaluación formativa en las áreas de lenguaje, matemáticas y ciencias naturales.</li>
+</ul>
+<!--  Organización de Estados Iberoamericanos OEI y Corpoeducación-->
+
+<h3>Organización de Estados Iberoamericanos <a id="tooltip-oei" href="http://www.oei.org.co/" data-toggle="tooltip" data-placement="bottom"  title="http://www.oei.org.co/">OEI</a> y <a href="http://corpoeducacion.org.co/" data-toggle="tooltip" data-placement="bottom" title="http://corpoeducacion.org.co/">Corpoeducación</a></h3>
+<div class="media-logos">
+  <div class="media-left-logo">
+    <a href="http://www.oei.org.co/">
+      <img class="lg-oei" src="https://dl.dropboxusercontent.com/u/575652037/edu-aliados/img/lg-oei.png" alt="logo-oei">
+    </a>
+  </div>
+  <div class="media-body-logos">
+	<label>Organización de Estados Iberoamericanos</label>
+  </div>
+</div>
+<div class="media-logos">
+  <div class="media-left-logo">
+    <a href="http://www.oei.org.co/">
+      <img class="lg-corpoe" src="https://dl.dropboxusercontent.com/u/575652037/edu-aliados/img/corpoeducacion.png" alt="logo-corpoeducacion">
+    </a>
+  </div>
+  <div class="media-body-logos">
+    <label>Corpoeducación</label>
+  </div>
+</div>
+<p>Las dos entidades, en esta alianza, aportan su experiencia y conocimiento, técnico, operativo y administrativo, al fortalecimiento de la gestión del proyecto desde sus diferentes las diferentes líneas de acción planteadas, constituyéndose en un equipo de trabajo que brinda una mirada local y regional sobre la educación. Así mismo, es la OEI quien realiza el seguimiento al ejercicio planteado.</p>
+
+<script>
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+</script>
+
+<!-- El compañamiento -->
+
+<p>La estrategia de acompañamiento busca generar las condiciones para el desarrollo de un trabajo pedagógico entre pares que analice fortalezas y oportunidades de cualificación institucional, en desarrollo de acciones específicas para el apoyo pedagógico entre un colegio mentor y una institución educativa acompañada.</p>
+<p>El acompañamiento <a href="#" data-toggle="tooltip" data-placement="bottom"  title="Instituciones Educativas Mentoras (características) Estas instituciones cuentan con resultados del nivel superior en las pruebas de Estado, al tiempo que son reconocidas por la cualificación de sus comunidades educativas de manera integral, unos currículos fortalecidos y proyectos trasversales que cuentan con la participación de diferentes actores educativos. Adicionalmente, son portadoras de una capacidad inigualable de establecer comunidades de aprendizaje en torno al mejoramiento del sistema educativo nacional.">Colegio mentor</a> – <a href="#" data-toggle="tooltip" data-placement="bottom"  title="Instituciones educativas Acompañadas (Características): Estas instituciones cuentan con niveles de desarrollo en la prueba de Estado que les remite a pensar con detenimiento sus procesos de formación, al punto que están dispuestas y abiertas a la interlocución con las instituciones mentoras. Son organizaciones escolares que escuchan, interiorizan y aportan desde su realidad y su contexto al mejoramiento de sus propias dinámicas de formación y de construcción de comunidad.">IE acompañada</a> se enmarca en un intercambio de conocimientos, experiencias y buenas prácticas para promover aprendizajes en doble vía: los dos colegios aprenden, se fortalecen, participan en un proceso de evaluación formativa y de esta manera, se esperan resultados que favorezcan el mejoramiento institucional en los dos establecimientos. <p>
+<h4>Los colegios mentores</h4>
+<p>De manera previa a especificar el rol de los colegios mentores, es importante resaltar el sentido del calificativo mentor, puesto que es relevante para este proceso. Mentor implica realizar una gestión que va más allá de un simple tutor, puesto que el mentor asesora, en este caso, en procesos de gestión pedagógica, con base en la experiencia y conocimientos que le han permitido alcanzar resultados de alto desempeño. La gestión del mentor no se queda solamente en asesorar, sino que trasciende hasta el punto de posibilitar nuevas oportunidades para su acompañado, para que sea factible concretar en el contexto real lo aprendido. En este sentido, es importante que el mentor tenga disposición para comunicar y compartir saberes y experiencias, habilidad para llegar a otras personas y competencias para el ejercicio de liderazgo, características que deben ser reconocidas por el acompañado.</p>
+<h4>Las IE acompañadas </h4>
+<p>son establecimientos educativos que han demandado acompañamiento como resultado del proceso de autoevaluación institucional mediante el cual han reconocido oportunidades para el mejoramiento y los ha lleva a tomar consciencia de la necesidad de una intervención externa. Es condición que estas IE tengan una matrícula superior a 500 estudiantes, hayan obtenido puntuación en un rango medio de la desviación estándar del promedio de la Prueba Saber 11 2015, a las cuales le fueron evaluados mínimo 15 estudiantes en dichas pruebas. Es importante que sea honesta en el análisis de los resultados de los aprendizajes de sus estudiantes y en el reconocimiento de las acciones desarrolladas y las que no han desarrollado, y que manifiesten voluntariamente su interés y voluntad para ser acompañadas por un colegio mentor par.</p>
+<script>
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+</script>
+
+<!-- Principios orientadores -->
+<div class="row">
+	<div class="col-md-4">
+		  <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="las diferentes acciones y decisiones en el marco del acompañamiento serán el resultado de acuerdos entre los colegios mentores y las IE acompañadas, las cuales se articularán con base en las prioridades establecidas entre los involucrados, hacia el logro de los objetivos propuestos en el plan de acompañamiento construido mutuamente, promoviendo la eficacia, la eficiencia, el seguimiento de las acciones, la implementación de planes de contingencia y evaluar los resultados, de acuerdo con las necesidades y eventualidades del acompañamiento.">Concentración</button>
+		  <button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" title="La estrategia de acompañamiento se aplica como un proceso desarrollado colaborativamente entre docentes y directivos docentes de los EE pares, compartiendo intereses y objetivos comunes concertados en el plan de acompañamiento, implementando acciones que faciliten la consecución de las metas definidas en el plan de acción correspondiente.">Colaboración</button>
+		  <button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" title=": para promover el mejoramiento de los aprendizajes en los niveles de secundaria y media mediante una estrategia de acompañamiento, en la que se fomenta el esfuerzo conjunto y organizado de los colegios mentores e IE acompañadas hacia el logro de los objetivos propuestos, compartiendo las responsabilidades y generando confianza recíproca entre los establecimientos educativos. ">Corresponsabilidad</button>
+	</div>
+	<div class="col-md-4">
+		  <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="las disertaciones durante el acompañamiento entre pares se fundamentan en el diálogo constructivo, escuchando atentamente. Esto implica que la participación en los diálogos de saberes será con actitud y disposición consciente para escuchar y comprender lo que docentes y directivos docentes comunican, al igual que para expresar sus propios aportes en el marco del respeto por las diferentes opiniones y abiertos a nuevas posibilidades.">Reflexión de la acción</button>
+		  <button type="button" class="btn btn-default"	data-toggle="tooltip" data-placement="bottom" title="En los diferentes encuentros entre pares se fomenta la reflexión entre lo que sucede, visionando lo que posiblemente sucederá, analizando lo que se hace y prospectando las mejores acciones que se puede ejecutar, con las debidas precauciones, debido a que los procesos en los que intervienen seres humanos, son siempre procesos complejos que implican permanente reflexión en torno a todos los elementos o componentes del proceso. ">Diálogo Constructivo</button>
+	</div>
+	<div class="col-md-4">
+		  <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Tooltip on left">Concentración</button>
+	</div>
+</div>
+<script>
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+</script>
+
+<!-- Variaciones variación popover -->
+
+<div class="row">
+	<div class="col-md-4">
+		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="Las diferentes acciones y decisiones en el marco del acompañamiento serán el resultado de acuerdos entre los colegios mentores y las IE acompañadas, las cuales se articularán con base en las prioridades establecidas entre los involucrados, hacia el logro de los objetivos propuestos en el plan de acompañamiento construido mutuamente, promoviendo la eficacia, la eficiencia, el seguimiento de las acciones, la implementación de planes de contingencia y evaluar los resultados, de acuerdo con las necesidades y eventualidades del acompañamiento.">
+			Concentración	
+		</button>
+		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="right" data-content="La estrategia de acompañamiento se aplica como un proceso desarrollado colaborativamente entre docentes y directivos docentes de los EE pares, compartiendo intereses y objetivos comunes concertados en el plan de acompañamiento, implementando acciones que faciliten la consecución de las metas definidas en el plan de acción correspondiente.">
+			Cooperación	
+		</button>
+		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="bottom" data-content="para promover el mejoramiento de los aprendizajes en los niveles de secundaria y media mediante una estrategia de acompañamiento, en la que se fomenta el esfuerzo conjunto y organizado de los colegios mentores e IE acompañadas hacia el logro de los objetivos propuestos, compartiendo las responsabilidades y generando confianza recíproca entre los establecimientos educativos.">
+			Corresponsabilidad	
+		</button>
+	</div>
+	<div class="col-md-4">
+		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="Las disertaciones durante el acompañamiento entre pares se fundamentan en el diálogo constructivo, escuchando atentamente. Esto implica que la participación en los diálogos de saberes será con actitud y disposición consciente para escuchar y comprender lo que docentes y directivos docentes comunican, al igual que para expresar sus propios aportes en el marco del respeto por las diferentes opiniones y abiertos a nuevas posibilidades.">
+			Reflexión de la acción
+		</button>
+		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="bottom" data-content="En los diferentes encuentros entre pares se fomenta la reflexión entre lo que sucede, visionando lo que posiblemente sucederá, analizando lo que se hace y prospectando las mejores acciones que se puede ejecutar, con las debidas precauciones, debido a que los procesos en los que intervienen seres humanos, son siempre procesos complejos que implican permanente reflexión en torno a todos los elementos o componentes del proceso.">
+			Diálogo Constructivo
+		</button>
+	</div>
+	<div class="col-md-4">
+		  <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Tooltip on left">Concentración</button>
+	</div>
+</div>
+<script>
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+</script>
