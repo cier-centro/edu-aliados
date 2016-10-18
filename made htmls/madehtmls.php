d9f86bc3771a3e82e14558848255a046cfae1192
Giovanni86 <giovanni.borda@gmail.com>
2016-10-18 16:26:56 -0500

    adelantos de la página

--- made htmls/madehtmls.php
+++ made htmls/madehtmls.php
@@ -82,20 +82,20 @@ $(function () {
 </script>
 
 <!-- Variaciones variación popover -->
-
-<div class="row">
-	<div class="col-md-4">
+<h4>Principios orientadores</h4>
+<div class="row infographic-content">
+	<div class="col-md-4 info-first-group">
 		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="Las diferentes acciones y decisiones en el marco del acompañamiento serán el resultado de acuerdos entre los colegios mentores y las IE acompañadas, las cuales se articularán con base en las prioridades establecidas entre los involucrados, hacia el logro de los objetivos propuestos en el plan de acompañamiento construido mutuamente, promoviendo la eficacia, la eficiencia, el seguimiento de las acciones, la implementación de planes de contingencia y evaluar los resultados, de acuerdo con las necesidades y eventualidades del acompañamiento.">
-			Concentración	
+			Concentración
 		</button>
 		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="right" data-content="La estrategia de acompañamiento se aplica como un proceso desarrollado colaborativamente entre docentes y directivos docentes de los EE pares, compartiendo intereses y objetivos comunes concertados en el plan de acompañamiento, implementando acciones que faciliten la consecución de las metas definidas en el plan de acción correspondiente.">
-			Cooperación	
+			Cooperación
 		</button>
 		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="bottom" data-content="para promover el mejoramiento de los aprendizajes en los niveles de secundaria y media mediante una estrategia de acompañamiento, en la que se fomenta el esfuerzo conjunto y organizado de los colegios mentores e IE acompañadas hacia el logro de los objetivos propuestos, compartiendo las responsabilidades y generando confianza recíproca entre los establecimientos educativos.">
-			Corresponsabilidad	
+			Corresponsabilidad
 		</button>
 	</div>
-	<div class="col-md-4">
+	<div class="col-md-4 info-second-group">
 		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="Las disertaciones durante el acompañamiento entre pares se fundamentan en el diálogo constructivo, escuchando atentamente. Esto implica que la participación en los diálogos de saberes será con actitud y disposición consciente para escuchar y comprender lo que docentes y directivos docentes comunican, al igual que para expresar sus propios aportes en el marco del respeto por las diferentes opiniones y abiertos a nuevas posibilidades.">
 			Reflexión de la acción
 		</button>
@@ -103,12 +103,202 @@ $(function () {
 			Diálogo Constructivo
 		</button>
 	</div>
-	<div class="col-md-4">
+	<div class="col-md-4 info-third-group">
 		  <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Tooltip on left">Concentración</button>
 	</div>
 </div>
+<label class="info-instruction">Has click sobre cada uno de los principios para conocer sus carácteristicas.</label>
 <script>
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
 </script>
+
+<!-- Actividades para el acompañamiento -->
+
+<h3>Actividades para el acompañamiento</h3>
+<div class="row infographic-moments">
+	<div class="col-md-4">
+		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="<ul class='list-unstyled'><li>Socialización de la estrategia en cada ETC.</li><li>Encuentro de alistamiento con colegios mentores.</li><li>Encuentro de alistamiento con IE acompañadas.</li><li>Encuentro de planeación entre los EE pares.</li><li>Socialización de la estrategia con la comunidad educativa.</li></ul>">
+			Actividades previas al acompañamiento situado
+		</button>
+	</div>
+	<div class="col-md-4">
+		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="<ul class='list-unstyled'><li>Ejecución del plan de acción</li><li>Acompañamiento y seguimiento a la implementación de los planes de acción</li><li>Interacción mediante la comunidad virtual de aprendizaje</li></ul>">
+			Desarrollo de las sesiones situadas
+		</button>
+	</div>
+	<div class="col-md-4">
+		<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="<ul class='list-unstyled'><li>Encuentro para el intercambio de experiencias</li><li>Socialización de la experiencia</li></ul>">
+			Actividades de socialización
+		</button>
+	</div>
+</div>
+<label class="info-instruction">Has click sobre cada uno de los principios para conocer sus carácteristicas.</label>
+<h4>Actividades previas al acompañamiento</h4>
+<h5>Socialización de la estrategia en cada ETC</h5>
+<p>Es una reunión que realiza el profesional de campo que dinamiza la implementación de la estrategia en ETC en coordinación con los profesionales responsables del proceso en cada Secretarías de Educación beneficiada. El propósito es socializar la estrategia con rectores de los establecimientos educativos pares focalizados, dar a conocer los parámetros de participación y establecer acuerdos y compromisos, por escrito, para la participación en el proyecto.</p>
+<img src="https://dl.dropboxusercontent.com/u/575652037/edu-aliados/img/accompanying-activities.jpg" class="img-responsive" alt="Responsive image">
+<h5>Socialización de la estrategia con la comunidad educativa</h5>
+<p>Corresponde a la gestión que realiza el rector de cada colegio mentor e IE acompañada para informar a su comunidad educativa sobre el proceso de acompañamiento en el que se ha comprometido el EE.</p>
+<h5>Encuentro de alistamiento con colegios mentores</h5>
+<p>Este encuentro busca socializar la estrategia de acompañamiento y el proyecto, a la vez que se realizan acuerdos iniciales con los colegios mentores para motivar su participación comprometida  y  se identifican sus características, expectativas y potencialidades para ejercer su rol de Mentor.</p>
+<img src="https://dl.dropboxusercontent.com/u/575652037/edu-aliados/img/colegios-mentores.jpg" class="img-responsive" alt="Responsive image">
+<h5>Encuentro de alistamiento con IE acompañadas</h5>
+<p>Este encuentro busca socializar la estrategia de acompañamiento y el proyecto, a la vez que se realizan acuerdos iniciales con las IE acompañadas para motivar su participación comprometida  y  se identifican sus características, expectativas, fortalezas y necesidades de acompañamiento.</p>
+<h5>Encuentro de planeación entre los EE pares</h5>
+<p>Corresponde al primer encuentro de planeación entre cada par de EE para formular el plan del acompañamiento, en el que los dos integrantes de la pareja de acompañamiento, con base en la caracterización realizada por cada uno de ellos, sus expectativas, fortalezas, necesidades de acompañamiento identificadas y con criterios de pertinencia al contexto sociocultural,  realizan acuerdos para el acompañamiento. </p>
+<img src="https://dl.dropboxusercontent.com/u/575652037/edu-aliados/img/encuentro-pares.jpg" class="img-responsive" alt="Responsive image">
+<h4>Desarrollo de las sesiones situadas</h4>
+<h5>Ejecución del plan de acción</h5>
+<p>Corresponde a las actividades específicas concertadas en el plan de acompañamiento, mediante la cual el colegio mentor y la IE acompañada se encuentran para llevar a cabo el acompañamiento en los tópicos priorizados en cada dimensión, conforme al plan de acción y cronograma.</p>
+<div class="row">
+  <div class="col-md-6">
+	<img src="https://dl.dropboxusercontent.com/u/575652037/edu-aliados/img/dia-clases.jpg" class="img-responsive" alt="Responsive image">
+  </div>
+  <div class="col-md-6">
+	<img src="https://dl.dropboxusercontent.com/u/575652037/edu-aliados/img/dia-clases2.jpg" class="img-responsive" alt="Responsive image">
+  </div>
+</div>
+<h5>Acompañamiento y seguimiento a la implementación de los planes de acción</h5>
+<p>Son las isitas de campo que realiza el profesional dinamizador del proceso en la ETC, mediante las cuales acompaña a cada par de EE en tres sesiones situadas durante el período de implementación de la estrategia. Estas visitas son para ofrecer asistencia técnica, proponer acciones de mejora, levantar información y hacer seguimiento a los recursos entregados para financiar el acompañamiento.</p>
+<img src="https://dl.dropboxusercontent.com/u/575652037/edu-aliados/img/el-salitre.jpg" class="img-responsive" alt="Responsive image">
+<h5>Interacción mediante la comunidad virtual de aprendizaje</h5>
+<p>El proyecto cuenta con una herramienta que se ha llamado “Comunidad virtual de aprendizaje” que permite el uso de las TIC para facilitar la interacción entre docentes, directivos docentes, equipo del proyecto Aliados 10, profesional de la SE y el Ministerio de Educación Nacional. El propósito es compartir conocimientos, experiencias, inquietudes, aclaraciones, material pedagógico, recibir y brindar asesoría técnica y hacer seguimiento a la ejecución de los recursos que financian el plan de acompañamiento.</p>
+<h5>Evaluación formativa</h5>
+<p>Como parte del seguimiento a la implementación de la estrategia de acompañamiento se realizará de manera permanente y transversal en los diferentes encuentros  y sesiones situadas en las que participa el profesional de campo dinamizador. Los resultados de esta evaluación permitirán identificar las fortalezas del proceso e identificar oportunamente situaciones especiales que requieren atención inmediata, ajustes al proceso o planes de contingencia.</p>
+<h4>Actividades de socialización</h4>
+<h5>Encuentro para el intercambio de experiencias</h5>
+<p>El encuentro se realiza con el fin de intercambiar las experiencias y vivencias de los EE mentores y acompañados, para identificar buenas prácticas y evaluar la implementación de la estrategia de acompañamiento. </p>
+<h5>Socialización</h5>
+<p>Es la oportunidad para que cada EE (colegio mentor e IE acompañada) informe a su comunidad educativa sobre los resultados de la participación en la implementación de la estrategia y para dar a conocer por diferentes medios los logros y aprendizajes alcanzados.</p>
+<script>
+$(function () {
+  $('[data-toggle="popover"]').popover({ html : true });
+});
+</script>
+<!-- Protagonistas / MEN -->
+
+<h3>Ministerio de Educación Nacional </h3>
+<div class="row">
+  <div class="col-md-3">
+    <a href="https://www.mineducacion.gov.co/">
+      <img class="lg-m" src="https://dl.dropboxusercontent.com/u/575652037/edu-aliados/img/lg-min-educacion.png" alt="logo-men">
+    </a>
+  </div>
+  <div class="col-md-9">
+	<p>Presta las condiciones económicas para el desarrollo del proyecto, en tanto una inversión que se destina a gastos de desplazamientos, propuestas para la formación de maestros, la adquisición de materiales didácticos y pedagógicos, entre otros requeridos. Así como en el apoyo de orden institucional que requieran las instituciones para el desarrollo de las actividades proyectadas.</p>
+  </div>
+</div>
+
+<!-- Protagonistas / Equipo de trabajo -->
+
+<h3>Equipo de trabajo </h3>
+<img class="img-responsive" alt="Formación Equipo Aliados 10" src="https://dl.dropboxusercontent.com/u/575652037/edu-aliados/img/formacion-equipo-a10.jpg">
+<label class="info-instruction">Formación Equipo Aliados 10</label>
+<table class="table-responsive">
+	<thead>
+		<tr>
+			<th colspan="2">DIRECTORIO EQUIPO CONVENIO  -ALIADOS 10 </th>
+		</tr>
+		<tr>
+			<td class="place-visible-column">CARGO</td>
+			<td class="place-visible-column">NOMBRE Y APELLIDO</td>
+		</tr>
+	</thead>
+	<tr>
+		<td>MEN - Gerente del Proyecto </td>
+		<td>Zohra Omar</td>
+	</tr>
+	<tr>
+		<td>OEI - Coordinación administrativa y financiera del Proyecto </td>
+		<td>Danit Torres</td>
+	</tr>
+	<tr>
+		<td>Corpoeducación - Apoyo a dirección del Proyecto </td>
+		<td>Carlos Javier Vargas</td>
+	</tr>
+	<tr>
+		<td>Corpoeducación - Apoyo pedagógico </td>
+		<td>Ginna Paola Herrera</td>
+	</tr>
+	<tr>
+		<td>Directora de Proyecto </td>
+		<td>Fulvia Lucero Valderrama</td>
+	</tr>
+	<tr>
+		<td>Prof Pedagógico </td>
+		<td>Maribel  Paez</td>
+	</tr>
+	<tr>
+		<td>Prof Pedagógico </td>
+		<td>Susana Leal Aponte </td>
+	</tr>
+	<tr>
+		<td>Asistente Dirección Proyecto</td>
+		<td>Marcela Pinzón</td>
+	</tr>
+	<tr>
+		<td>Profesional de Campo Bogotá</td>
+		<td>Claudia Esperanza  Aparicio </td>
+	</tr>
+	<tr>
+		<td>Profesional de Campo Bogotá</td>
+		<td>Stella Angarita Pinzón </td>
+	</tr>
+	<tr>
+		<td>Profesional de Campo Antioquia</td>
+		<td>Carlos Andrés Galán Trujillo</td>
+	</tr>
+	<tr>
+		<td>Profesional de Campo Atlántico - Bolivar</td>
+		<td>Mena  Beatriz  Torres </td>
+	</tr>
+	<tr>
+		<td>Profesional de Campo Risaralda - Caldas</td>
+		<td>Emma Castañeda Alvarez </td>
+	</tr>
+	<tr>
+		<td>Profesional de Campo Boyacá - Cundinamarca</td>
+		<td>Yolanda Vasquez</td>
+	</tr>
+	<tr>
+		<td>Profesional de Campo Atlántico - Bolivar</td>
+		<td>Marly Torres</td>
+	</tr>
+	<tr>
+		<td>Profesional de Campo Santanderes</td>
+		<td>Maria Jimena Arevalo</td>
+	</tr>
+	<tr>
+		<td>Profesional de Campo Valle - Armenia</td>
+		<td>María Fernanda Delgado Portela </td>
+	</tr>
+	<tr>
+		<td>Profesional de sistematización </td>
+		<td>Pedro Garzón </td>
+	</tr>
+	<tr>
+		<td>Apoyo de sistematización  </td>
+		<td>Karen Ortiz</td>
+	</tr>
+</table>
+<!-- Documentos de interés -->
+<h3>Documentos de interés</h3>
+<p>Para Aliados 10 es una premisa el diálogo de saberes, por lo que a continuación se comparten documentos de interés para los interesados en esta experiencia, así como otras referencias que sin duda, aportan a la comprensión del horizonte de sentido que adquiere el ejercicio.</p>
+<div class="content-docs">
+	<div class="documents-container">
+		<a target="_blank" href="http://www.mutisschool.com/portal/Formatos%20y%20Documentos%20Capacitacion%20Docentes/ESTRATEGIAPEDCorr.pdf" class="lbl-documents">Camacho Carantón, Tania y otros. 2012. Estrategias pedagógicas en el ámbito educativo.</a>
+	</div>
+	<div class="documents-container">
+		<a target="_blank" href="https://www.gov.uk/government/organisations/national-college-for-teaching-and-leadership." class="lbl-documents">Hill, R., Matthews, P. (2010). Schools leading schools II: the growing impact of National Leaders of Education. National College for Leadership of Schools and Children Services.</a>
+	</div>
+	<div class="documents-container">
+		<a target="_blank" href="https://www.gov.uk/government/organisations/national-college-for-teaching-and-leadership." class="lbl-documents">Mesa interinstitucional del CNE, integrada por ANGR, CNE, MCLCP, Promeb-ACDI, SUMA-USAID, UARM, UNICEF. 2012. HOJA INFORMATIVA N° 3: ¿Cómo se desarrolla el proceso de Acompañamiento Pedagógico?</a>
+	</div>
+	<div class="documents-container">
+		<a target="_blank" href="http://cambridge.academia.edu/MarianoRosenzvaig" class="lbl-documents">Rosenzvaig M. Directores exitosos como agentes de cambio: ampliando las oportunidades de mejoramiento escolar.</a>
+	</div>
+</div>
+<!-- menú Secundario -->
+<h1></h1>
